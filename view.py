d91b79b030010ef2a33b12942081ff42032ebeda
kamandkrg <kamandkargar@gmail.com>
2021-12-09 13:45:47 +0330

    add callback to Panel

--- view.py
+++ view.py
@@ -2,5 +2,14 @@ from configure import Label, Frame, LabelFrame, TopLevel, Scale, Button, Entry, 
 
 
 class PanelManager(Tk):
-    def __init__(self, callback_show_patient, callback_add_patient, callback_add_ambulance):
+    def __init__(self, callback_show_patient, callback_add_patient, callback_add_ambulance, callback_on,
+                 callback_off):
         super(PanelManager, self).__init__()
+        self.callback_show_patient = callback_show_patient
+        self.call_back_add_patient = callback_add_patient
+        self.callback_add_ambulance = callback_add_ambulance
+        self.callback_on = callback_on
+        self.callback_off = callback_off
+
+
+
