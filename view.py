cd6501f4a9b66d81c45f90bfa33f75fd19703b70
kamandkrg <kamandkargar@gmail.com>
2021-12-10 16:52:16 +0330

    fix bug

--- view.py
+++ view.py
@@ -1,4 +1,4 @@
-from configure import Label, Frame, LabelFrame, TopLevel, Scale, Button, Entry, Tk
+from configure import Label, Frame, LabelFrame, Scale, Button, Entry, Tk
 from tkinter import messagebox, ttk
 from panel_ambulance import PanelAmbulance
 from AddInfo import AddInfo
@@ -9,7 +9,7 @@ class PanelManager(Tk):
                  callback_off, callback_clock, callback_choice, callback_show_am):
         super(PanelManager, self).__init__()
         self.callback_show_patient = callback_show_patient
-        self.call_back_add_patient = callback_add_patient
+        self.callback_add_patient = callback_add_patient
         self.callback_add_ambulance = callback_add_ambulance
         self.callback_on = callback_on
         self.callback_choice = callback_choice
@@ -32,7 +32,7 @@ class PanelManager(Tk):
         frm_lbl.grid(row=0, column=1)
 
         frm1 = Frame(frm_lbl)
-        frm1.grid(row=0, column=0)
+        frm1.grid(row=1, column=0)
         Button(frm1, text="Add Patient", command=self.add_patient).grid(row=0, column=0)
         Button(frm1, text="Add Ambulance", command=self.add_ambulance).grid(row=0, column=1)
 
@@ -43,7 +43,7 @@ class PanelManager(Tk):
         Label(self.frm2, text="Speed:").grid(row=0, column=2)
         self.ent_speed = Scale(self.frm2, width=18, length=122, from_=10, to=200, orient="horizontal")
         self.ent_speed.grid(row=0, column=3)
-        Button(self.frm2, text="Ok", command=self.forget_grid_frm2).grid(row=0, column=4)
+        Button(self.frm2, text="Ok", command=self.forget_grid_frm3).grid(row=0, column=4)
 
         self.frm3 = Frame(frm_lbl)
         Label(self.frm3, text="Name:").grid(row=0, column=0)
@@ -52,24 +52,25 @@ class PanelManager(Tk):
         Label(self.frm3, text="Worse:").grid(row=0, column=2)
         self.ent_worse = Scale(self.frm3, width=18, length=122, from_=0, to=200, orient="horizontal")
         self.ent_worse.grid(row=0, column=3)
-        Button(self.frm3, text="Ok", command=self.forget_grid_frm3).grid(row=0, column=4)
+        Button(self.frm3, text="Ok", command=self.forget_grid_frm2).grid(row=0, column=4)
 
-        Label(frm_lbl, text="Patient").grid(row=0, column=2)
+        Label(frm_lbl, text="Patient").grid(row=2, column=0)
         frm4 = Frame(frm_lbl)
-        frm4.grid(row=0, column=3)
+        frm4.grid(row=3, column=0)
         self.tree = ttk.Treeview(frm4, show="headings", selectmode="browse", height=10)
         self.tree["columns"] = ("name", "worse")
         self.tree.heading("name", text="Name")
         self.tree.heading("worse", text="Worse")
         self.tree.grid(row=0, column=0)
 
-        Label(frm_lbl, text="Ambulance").grid(row=0, column=2)
+        Label(frm_lbl, text="Ambulance").grid(row=4, column=0)
         frm5 = Frame(frm_lbl)
-        frm5.grid(row=0, column=4)
+        frm5.grid(row=5, column=0)
         self.tree1 = ttk.Treeview(frm5, show="headings", selectmode="browse", height=10)
-        self.tree1["columns"] = ("name", "speed")
+        self.tree1["columns"] = ("name", "speed", "mission")
         self.tree1.heading("name", text="Name")
-        self.tree1.heading("worse", text="Speed")
+        self.tree1.heading("speed", text="Speed")
+        self.tree1.heading("mission", text="Mission")
         self.tree1.grid(row=0, column=0)
         self.tree1.bind("<Double-1>", self.panel_am)
 
@@ -80,31 +81,34 @@ class PanelManager(Tk):
         self.not_tab.add(panel, text="Ambulance")
 
     def on_mission(self):
-        am_res, pat_res = self.callback_choice
+        am_res, pat_res = self.callback_choice()
         result = messagebox.askokcancel("Do you want?", f"Do you want {am_res.name} to follow {pat_res.name}")
         if result:
-            self.callback_on(pat_res)
+            self.callback_on()
             address = AddInfo(self, "Address:", f"{pat_res.name}")
             phone = AddInfo(self, "Phone:", f"{pat_res.name}")
             pat_res.update_patient(address, phone)
-
-    def forget_grid_frm3(self):
-        self.frm3.grid_forget()
-        self.call_back_add_patient(self.name, self.speed_worse)
+        self.show_patient()
         self.show_ambulance()
 
-    def forget_grid_frm2(self):
+    def forget_grid_frm3(self):
+        self.name, self.speed_worse = self.ent_name_am.get(), self.ent_speed.get()
+        self.callback_add_ambulance(self.name, int(self.speed_worse))
+        self.show_ambulance()
         self.frm2.grid_forget()
-        self.callback_add_ambulance(self.name, self.speed_worse)
+
+    def forget_grid_frm2(self):
+        self.name, self.speed_worse = self.ent_name_pat.get(), self.ent_worse.get()
+        self.callback_add_patient(self.name, int(self.speed_worse))
         self.show_patient()
+        self.frm3.grid_forget()
+        self.on_mission()
 
     def add_ambulance(self):
-        self.frm2.grid(row=0, column=1)
-        self.name, self.speed_worse = self.ent_name_am.get(), self.ent_speed.get()
+        self.frm2.grid(row=1, column=0)
 
     def add_patient(self):
-        self.frm3.grid(row=0, column=1)
-        self.name, self.speed_worse = self.ent_name_pat.get(), self.ent_worse.get()
+        self.frm3.grid(row=1, column=0)
 
     def show_patient(self):
         patients = self.callback_show_patient()
@@ -119,18 +123,17 @@ class PanelManager(Tk):
 
     def show_ambulance(self):
         ambulance_off, ambulance_on = self.callback_show_am()
-        self.tree1.delete(*self.tree.get_children())
+        self.tree1.delete(*self.tree1.get_children())
         count = 0
         self.list_am = []
         for am in ambulance_off.traverse():
-            a = (am.name, am.speed)
+            a = (am.name, am.speed, "off")
             self.list_am.append(am)
             self.tree1.insert("", "end", values=a, text=str(count))
             count += 1
-
         for am in ambulance_on.traverse():
             self.list_am.append(am)
-            a = (am.name, am.speed)
+            a = (am.name, am.speed, "on")
             self.tree1.insert("", "end", values=a, text=str(count))
             count += 1
 
