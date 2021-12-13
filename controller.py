29902f5ac0cfbd7e3678cecfb4bd119bf5e821fe
kamandkrg <kamandkargar@gmail.com>
2021-12-13 17:52:40 +0330

    completing controller

--- controller.py
+++ controller.py
@@ -1,12 +1,20 @@
 from model import Core
 from view import PanelManager
+from pickle import load, dump
+from os.path import exists as file_exists
 
-database = Core()
+if file_exists("database.bin"):
+    file = open("database.bin", "rb")
+    database = load(file)
+    file.close()
+else:
+    database = Core()
 
 win = PanelManager(database.show_patient, database.add_patient, database.add_ambulance, database.on_mission,
                    database.off_mission, database.clock, database.choice_ambulance, database.show_ambulance)
 
 win.mainloop()
 
-
-
+file = open("database.bin", "wb")
+dump(database, file)
+file.close()
